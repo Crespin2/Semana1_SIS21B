df7fcfe80207e2998240c353563297f3d642144d
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-03-10 11:37:50 -0600

    Calculo de monedas

--- src/Ejercicios/CalculoDeMonedas.java
+++ src/Ejercicios/CalculoDeMonedas.java
@@ -3,8 +3,10 @@ package Ejercicios;
 public class CalculoDeMonedas {
     public static void main(String[] args) {
         //Declarar Variables
-        double MEuropa, MReinoUnido, MAustralia, MCanada;
-        
+        double MEuropa, MReinoUnido, MAustralia, MCanada, USD;
+        USD=100;
+        MEuropa=USD*0.70;
+        MReinoUnido=0.61
     }
     
 }

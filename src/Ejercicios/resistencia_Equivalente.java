741ecd545d2a9b2beb898de3f734ccc611ca2787
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-03-10 11:14:08 -0600

    Resistencia Equivalente

--- src/Ejercicios/resistencia_Equivalente.java
+++ src/Ejercicios/resistencia_Equivalente.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ejercicios;
 
-/**
- *
- * @author ITCA
- */
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+
 public class resistencia_Equivalente {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declarar Variables
+        double ResistenciaEq, r1, r2, r3, r4;
+        
+        //Inicializar variables
+        r1=5.1;
+        r2=68;
+        r3=75;
+        r4=82;
+         NumberFormat formato = new DecimalFormat("#0.00000");
+        //Uso de las variables
+        ResistenciaEq=1/((r1/1)+(r2/1)+(r3/1)+(r4/1));
+        
+        //Imprimir resultado
+        System.out.println("La resistencia equivalente es: "+formato.format(ResistenciaEq)+" ohm.");
+        
     }
+    
     
 }

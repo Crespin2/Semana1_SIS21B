9754ad288b3fb99058129e2c48bda421523b43c4
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-03-10 10:23:15 -0600

    Calculo interés Simple

--- src/Ejercicios/CalculoInteresSimple.java
+++ src/Ejercicios/CalculoInteresSimple.java
@@ -4,5 +4,17 @@ public class CalculoInteresSimple {
     public static void main(String[] args) {
         //Declarar Variables
         double interes, capitalPrestado, tiempo, tasaDeInteres, tiempoEnMeses;
+        
+        //Inicializar Variables
+        interes=144;
+        capitalPrestado=1200.00;
+        tasaDeInteres=0.08;
+        
+        //Uso de variables
+        tiempo=interes/(capitalPrestado * tasaDeInteres);
+        tiempoEnMeses=tiempo*12;
+        
+        System.out.println("Tiempo: "+tiempo+ " años.");
+        System.out.println("Tiempo: "+tiempoEnMeses+ " meses.");
     }
 }

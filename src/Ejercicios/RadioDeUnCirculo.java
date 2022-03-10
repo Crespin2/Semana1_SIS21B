ef772daa438c2ecc5f16c3c3690ff28be55755c7
Crespin2 <jonathan.molina21@itca.edu.sv>
2022-03-10 10:37:32 -0600

    radio de un circulo con Java

--- src/Ejercicios/RadioDeUnCirculo.java
+++ src/Ejercicios/RadioDeUnCirculo.java
@@ -1,16 +1,20 @@
 package Ejercicios;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class RadioDeUnCirculo {
     public static void main(String[] args) {
         //Declarar Variables
         double area, radio;
         final double pi=3.1416; //la palabra FINAL significa que el valor de l avariable no cambiara
         
+        NumberFormat formato = new DecimalFormat("#0.00");
         //Inicializacion de Variables
         area=145;
         
         //Uso de Variables
         radio=Math.sqrt(area/pi);//calcular la raiz cuadrada con la funcion Math.Sqrt
-        System.out.println("Radio: "+radio+ " cm");
+        System.out.println("Radio: "+formato.format(radio)+ " cm");
     } 
 }

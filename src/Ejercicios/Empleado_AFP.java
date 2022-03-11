89076e687db02abdbedeedbf5b8e5c0761d6c451
Crespin2 <101343368+Crespin2@users.noreply.github.com>
2022-03-11 11:04:56 -0600

    Calculo de pago a la AFP

--- src/Ejercicios/Empleado_AFP.java
+++ src/Ejercicios/Empleado_AFP.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicios;
 
-/**
- *
- * @author Jonathan
- */
 public class Empleado_AFP {
+    public static void main(String[] args) {
+        //Declarar Variable
+        double desEmpleado, desEmpleador,TPagar;
+        
+        //Inicializar varibles
+        desEmpleado=500*0.06;
+        desEmpleador=500*0.065;
+        TPagar=desEmpleado+desEmpleador;
+        
+        //Imprimir resultado
+        System.out.println("El trabajor estara pagando: $"+TPagar+" a la AFP en concepto de Empleado y de Empleador");
+        
+    }
     
 }
